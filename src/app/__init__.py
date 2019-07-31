3772cce2d0a5bc2edbd97c7f983d1da9da49dee3
pogzyb <pogzyb@umich.edu>
2019-07-30 23:02:46 -0400

    Fix context of TaskManager

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,21 +2,31 @@
 from flask import Flask
 
 
+db = None
+tm = None
+
+
 # app factory function
 def create_app():
     global db
+    global tm
     # initialize flask instance
     app = Flask(__name__, instance_relative_config=True)
     # configs from configs.py
     app.config.from_pyfile('config.py')
     with app.app_context():
-        # register views
-        from . import views
-        app.register_blueprint(views.homepage)
-        app.register_blueprint(views.api)
         # initialize database
         from .models import db
         db.init_app(app)
         db.create_all()
+        # initialize task manager
+        from .tasks import TaskManager
+        tm = TaskManager()
+        tm.init_app(app, db)
+        # register views
+        from .views import webpage, api
+        app.register_blueprint(webpage)
+        app.register_blueprint(api)
+
     return app
 
