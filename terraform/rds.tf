34d32f83d21a0b84973b82d278c3bc852ef4f786
pogzyb <pogzyb@umich.edu>
2019-08-01 09:38:59 -0400

    Update RDS instance configs

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,6 +1,6 @@
 /********
 
-terraform/prereqs/rds.tf contains all the necessary resources to
+terraform/rds.tf contains all the necessary resources to
 setup the RDS postgres database for the ECS application
 
 Resources:
@@ -9,20 +9,6 @@ Resources:
 - Security Groups
 
 *********/
-
-# create the RDS instance
-resource "aws_db_instance" "fp-rds" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "11"
-  instance_class       = "db.t2.micro"
-  name                 = ""
-  username             = ""
-  password             = ""
-  parameter_group_name = ""
-  db_subnet_group_name = aws_db_subnet_group.fp-db-subnet.name
-}
 
 # create security group
 resource "aws_security_group" "rds-db-sg" {
@@ -44,4 +30,25 @@ resource "aws_security_group" "rds-db-sg" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# create the RDS instance
+resource "aws_db_instance" "fp-rds" {
+  allocated_storage           = 20
+  storage_type                = "gp2"
+  engine                      = "postgres"
+  engine_version              = "11"
+  instance_class              = var.rds_instance_type
+  name                        = "postgresdb"
+  username                    = "root"
+  password                    = "admin123"
+  port                        = var.postgres_db_port
+  vpc_security_group_ids      = [aws_security_group.rds-db-sg.id]
+  parameter_group_name        = "default.postgres11"
+  db_subnet_group_name        = aws_db_subnet_group.fp-db-subnet.name
+  publicly_accessible         = false
+  allow_major_version_upgrade = false
+  auto_minor_version_upgrade  = false
+  apply_immediately           = true
+  storage_encrypted           = false
 }
