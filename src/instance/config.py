3563a372da7c4509bb829de7adfb865afe542338
Joe <pogzyb@umich.edu>
2020-01-25 16:49:31 -0500

    Move logger out of config.py

--- src/instance/config.py
+++ src/instance/config.py
@@ -1,38 +1,13 @@
-# config.py
+# instance/config.py
 import os
-import logging
 
-
-# SECRET KEY
 SECRET_KEY = os.getenv('APP_SECRET_KEY')
-
-
-# Base Directory
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
-
-
-# Logging
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-logging.getLogger(__name__).setLevel(logging.INFO)
-
-
-# Construct DB Connection URI
-if os.getenv('FLASK_ENV') == 'production':
-    SQLALCHEMY_DATABASE_URI = \
-        f'postgres://' \
-        f'{os.getenv("POSTGRES_USER")}:' \
-        f'{os.getenv("POSTGRES_PASSWORD")}@' \
-        f'{os.getenv("POSTGRES_ENDPOINT")}/' \
-        f'{os.getenv("POSTGRES_DATABASE")}'
-else:
-    SQLALCHEMY_DATABASE_URI = \
-        f'postgres://' \
-        f'{os.getenv("POSTGRES_USER")}:' \
-        f'{os.getenv("POSTGRES_PASSWORD")}@' \
-        f'{os.getenv("POSTGRES_HOSTNAME")}:' \
-        f'{os.getenv("POSTGRES_PORT")}/' \
-        f'{os.getenv("POSTGRES_DATABASE")}'
-
-
-# Suppress Warnings
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = \
+    f'postgres://' \
+    f'{os.getenv("POSTGRES_USER")}:' \
+    f'{os.getenv("POSTGRES_PASSWORD")}@' \
+    f'{os.getenv("POSTGRES_HOSTNAME")}:' \
+    f'{os.getenv("POSTGRES_PORT")}/' \
+    f'{os.getenv("POSTGRES_DATABASE")}'
