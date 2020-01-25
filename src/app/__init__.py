f5c5dc0144544ba0aef338c24b46a8e9ba48a031
Joe <pogzyb@umich.edu>
2020-01-25 16:48:20 -0500

    Re-worked the __init__ factory
    globals were swapped out for .init_app() methods
    for both tm and db

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,35 +1,22 @@
 # app/__init__.py
 from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
+import logging
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.getLogger(__name__).setLevel(logging.INFO)
 
 
-db = None
-tm = None
-
-
-# app factory function
 def create_app():
-    global db
-    global tm
-    # initialize flask instance
     app = Flask(__name__, instance_relative_config=True)
-    # configs from configs.py
     app.config.from_pyfile('config.py')
+    from app.models import db
+    db.init_app(app)
+    from app.tasks import tm
+    tm.init_app(app, db)
     with app.app_context():
-        # proxy-fix for aws application load-balancer headers
-        app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_for=1, x_host=1)
-        # initialize database
-        from .models import db
-        db.init_app(app)
         db.create_all()
-        # initialize task manager
-        from .tasks import TaskManager
-        tm = TaskManager()
-        tm.init_app(app, db)
-        # register views
-        from .views import webpage, api
-        app.register_blueprint(webpage)
-        app.register_blueprint(api)
-
+        from app.blueprints import web, api
+        app.register_blueprint(web)
+        app.register_blueprint(api, url_prefix='/api')
     return app
 
